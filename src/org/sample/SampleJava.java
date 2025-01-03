180cbd05dfd6138557a5a35663cee784df33f80a
RamkumarV09 <ramkumarv0986@gmail.com>
2025-01-03 21:49:36 +0530

    Second Practice GIT

---
+++ src/org/sample/SampleJava.java
@@ -0,0 +1,21 @@
+package org.sample;
+
+public class SampleJava {
+
+	
+	private void meth1() {
+
+		System.out.println("Meth 1");
+	}
+	
+	private void meth2() {
+		
+		System.out.println("Meth 2");
+
+	}
+	
+	private void meth3() {
+		System.out.println("Meth 3");
+
+	}
+}
